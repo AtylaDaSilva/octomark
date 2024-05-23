96e049f1139d2025f9a6e8cf8bd1e843b06e67e6
Atyla da Silva <102058935+AtylaDaSilva@users.noreply.github.com>
2024-05-23 18:43:37 -0300

    MVP WIP 13
    * Added emoji category tabs in emoji menu.
    * Updated emoji list structure

--- src/components/icons/EmojiIcon.tsx
+++ src/components/icons/EmojiIcon.tsx
@@ -12,12 +12,17 @@ import { handleCommand, emoji } from "@/commands";
 import { CommandBarProps } from "../CommandBar";
 
 // Emoji list
-import emojis from "@/utils/emojis.json";
+import emojiList from "@/utils/emojis.json";
 
 export default function EmojiIcon(props: CommandBarProps) {
     const [anchorElement, setAnchorElement] = useState<null | HTMLElement>(null);
+    const [activeTab, setActiveTab] = useState<number>(0);
     const isOpen = Boolean(anchorElement);
-    
+
+    const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+        setActiveTab(newValue);
+    };
+
     const handleClick = (event: React.MouseEvent<HTMLElement>) => {
         setAnchorElement(event.currentTarget);
     };
@@ -34,6 +39,28 @@ export default function EmojiIcon(props: CommandBarProps) {
             props.markdown,
             props.setMarkdown
         );
+        handleClose();
+        setActiveTab(0);
+    }
+
+    function EmojiPanel({ emojis, activeTab, tabIndex }: { emojis: any[], activeTab: number, tabIndex: number }) {
+        const panel = emojis.map(emoji => {
+            return (
+                activeTab === tabIndex && (
+                    <IconButton
+                        key={emoji.code[0]}
+                        hidden={activeTab !== tabIndex}
+                        size="small"
+                        color="primary"
+                        onClick={() => handleMenuItemClick(emoji.code[0])}
+                    >
+                        {emoji.ico}
+                    </IconButton>
+                )
+            );
+        })
+
+        return panel;
     }
 
     return (
@@ -48,24 +75,28 @@ export default function EmojiIcon(props: CommandBarProps) {
                 anchorEl={anchorElement}
                 open={isOpen}
                 onClose={handleClose}
+                sx={{ overflowY: "auto", minWidth: "1485px", maxWidth: "1485px" }}
             >
                 <Grid
                     container
                     padding={1}
                 >
+                    <Grid item xs={12}>
+                        <Tabs
+                            value={activeTab}
+                            onChange={handleTabChange}
+                            aria-label="Emoji Tabs"
+                        >
+                            {
+                                emojiList.map((emoji) => {
+                                    return <Tab key={emoji.categoryName} label={emoji.categoryIcon} />
+                                })
+                            }
+                        </Tabs>
+                    </Grid>
                     {
-                        emojis.map(emoji => {
-                            return (
-                                <Grid item key={emoji.code}>
-                                    <IconButton
-                                        size="small"
-                                        color="primary"
-                                        onClick={() => handleMenuItemClick(emoji.code)}
-                                    >
-                                        {emoji.ico}
-                                    </IconButton>
-                                </Grid>
-                            );
+                        emojiList.map((emoji, index) => {
+                            return <EmojiPanel key={index} emojis={emoji.emojis} activeTab={activeTab} tabIndex={index} />
                         })
                     }
                 </Grid>
