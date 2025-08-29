14223e09019df50ac0ba111fb91c120ca085cea7
Atyla-da-Silva-Solab <dev.atyla@solab.tec.br>
2025-08-29 14:33:29 -0300

    FEAT hide tooltip on preview close/open

--- src/components/icons/HideShowPreviewIcon.tsx
+++ src/components/icons/HideShowPreviewIcon.tsx
@@ -2,6 +2,9 @@
 import { IconButton, Tooltip } from "@mui/material";
 import { Visibility, VisibilityOutlined } from "@mui/icons-material";
 
+//React
+import { useState } from "react";
+
 // Types
 import { TState, } from "@/types";
 
@@ -11,17 +14,26 @@ import { DEFAULT_ICON_SIZE, SHOW_PREVIEW_LOCAL_STORAGE_KEY } from "@/utils/const
 export default function HideShowPreviewIcon({ showPreview, setShowPreview }: TState) {
     const PreviewIcon = showPreview ? Visibility : VisibilityOutlined
     const label = `${showPreview ? 'Hide' : 'Show'} Preview`
+    const [isTooltipOpen, setIsTooltipOpen] = useState<boolean>(false)
     return (
-        <Tooltip title={label}>
+        <Tooltip
+            title={label}
+            open={isTooltipOpen}
+            onOpen={() => setIsTooltipOpen(true)}
+            onClose={() => setIsTooltipOpen(false)}
+        >
             <IconButton
                 aria-label={`Button | ${label}`}
                 color="info"
                 onClick={() => {
                     if (setShowPreview) {
+                        // Save preview state to local storage
                         setShowPreview(currState => {
                             localStorage.setItem(SHOW_PREVIEW_LOCAL_STORAGE_KEY, JSON.stringify(!currState));
                             return !currState
                         });
+                        // Hide tooltip
+                        setIsTooltipOpen(false);
                     }
                 }}
             >
