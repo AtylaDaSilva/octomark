bf237e0b2186ebced68952bb0821b3fffe238da5
Atyla-da-Silva-Solab <dev.atyla@solab.tec.br>
2025-09-04 12:05:25 -0300

    UPDATE new app title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,8 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 export const metadata: Metadata = {
-  title: "OctoMark | Markdown Editor",
-  description: "CommonMark and GitHub-Flavored Markdown Editor. ",
+  title: "OctoMark | A GitHub-Flavored Markdown Editor",
+  description: "OctoMark | A GitHub-Flavored Markdown Editor",
 };
 
 export default function RootLayout({
